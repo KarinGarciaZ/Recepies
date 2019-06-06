51fd428985990327b9d7be77e5c46af10969be36
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-05 19:11:40 -0500

    Passing data among components with events.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -13,9 +13,15 @@ export class RecipeListComponent implements OnInit {
     new Recipe( 'Anvorgueza', 'anvorgueza hawaiana', 'https://www.elgrafico.mx/sites/default/files/styles/f5-689x388/public/2019/05/28/dia_de_la_hamburgesa_anvorgesa_origen_memes.jpg?itok=tl5gZg0u' )
   ]
 
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectItem( i: number ) {
+    this.recipeSelected.emit(this.recipes[i])
   }
 
 }
