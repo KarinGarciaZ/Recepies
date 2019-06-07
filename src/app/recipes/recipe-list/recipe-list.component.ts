b5095baecd8f8b650934d81b7b4a9a0a2fd123b4
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-07 11:49:16 -0500

    Sevice injected to get recipes.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,16 +9,14 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [
-    new Recipe( 'tacos', 'tacos de canasta', 'https://cocina-casera.com/mx/wp-content/uploads/2018/05/tacos-de-canasta-700x400.jpg' ),
-    new Recipe( 'Anvorgueza', 'anvorgueza hawaiana', 'https://www.elgrafico.mx/sites/default/files/styles/f5-689x388/public/2019/05/28/dia_de_la_hamburgesa_anvorgesa_origen_memes.jpg?itok=tl5gZg0u' )
-  ]
+  recipes: Recipe[];
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor( private recipeService: RecipeService ) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   selectItem( i: number ) {
