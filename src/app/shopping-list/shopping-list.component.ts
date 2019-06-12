b7202e903ffdaa7194a74fd17a07daafd4762883
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-12 18:37:40 -0500

    Added routing for new component.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from './ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,15 +11,20 @@ import { ShoppingListService } from './shopping-list.service';
 export class ShoppingListComponent implements OnInit {
 
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
 
   constructor( private ingredientService: ShoppingListService ) { }
 
   ngOnInit() {
     this.ingredients = this.ingredientService.getIngredients()
 
-    this.ingredientService.ingredientsChanged
+    this.igChangeSub = this.ingredientService.ingredientsChanged
       .subscribe( ( newArray: Ingredient[] ) => {
         this.ingredients = newArray
       })
+  }
+
+  OnDestroy() :void{
+    this.igChangeSub.unsubscribe();
   }
 }
