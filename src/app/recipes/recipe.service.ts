369a7f660fe2de124edf3439130b5a39e7c4418e
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-12 09:20:48 -0500

    function added to return a recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -34,4 +34,8 @@ export class RecipeService {
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppinglistService.addIngredients(ingredients)
   }
+
+  getRecipe( index: number ) {
+    return this.recipes[index]
+  }
 }
